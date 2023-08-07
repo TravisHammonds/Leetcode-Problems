a4329a3eba8ebdff5db12ee4df80a0eda8bc3a3c
Travis Hammonds <travishammonds2@gmail.com>
2023-08-06 22:52:44 -0400

    Time: 61 ms (57.37%), Space: 41.6 MB (94.5%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,16 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let prefix = []
-    for(let i=0; i< strs[0].length; i++){
-      let letter = strs[0].charAt(i);
-      for(let j=1; j<strs.length; j++){
-        if(strs[j].charAt(i) !== letter){
-          return prefix.join("");
+    if (strs.length === 0) {
+        return "";
+    }
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);
         }
-      } 
-        prefix.push(letter);
-      }
-    return prefix.join("");
-}
+    }
+
+    return prefix;
+};
+
 
