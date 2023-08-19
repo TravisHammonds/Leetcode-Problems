104e838f191f57f5a779e368be29a2ab1c0bfbf6
Travis Hammonds <travishammonds2@gmail.com>
2023-08-19 13:37:54 -0400

    Time: 46 ms (99.44%), Space: 44.1 MB (53.97%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(head == null) return null;
+    let curr = head
+    while(curr.next != null) {
+        if(curr.val == curr.next.val){
+            let next_next = curr.next.next;
+            delete(curr.next);
+            curr.next = next_next; 
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head; 
+};
